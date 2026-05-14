65fbbf8e7af7d90eada2537afe24a4d0cc47841a
wwsalahww123-tech <wwsalahww123@gmail.com>
2026-05-14 06:13:55 +0200

    Update Untitled-2.py

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,47 +1,85 @@
-print("--- Task 1: Family Search ---")
-# Making lists to store data
-names = []
-ages = []
-n = int(input("How many members in your family? "))
-# Getting data from the user
-for i in range(n):
-    name_input = input("Enter name: ")
-    age_input = input("Enter age: ")
-    names.append(name_input)
-    ages.append(age_input)
-# The search part
-key = input("\nEnter the name you want to find: ")
-found = False
-for i in range(len(names)):
-    if names[i].lower() == key.lower():
-        print(names[i] + " was found and is aged " + str(ages[i]))
-        found = True
-        break
-if found == False:
-    print("Not found")
-# Question 2: Bubble Sort for Names
-print("\n--- Task 2: Sorting Names ---")
-my_names = ["Zuck", "Shayd", "Emely", "Amjed", "Esther", "Manar", "Anne"]
-# Bubble sort logic
-for i in range(len(my_names)):
-    for j in range(0, len(my_names) - i - 1):
-        if my_names[j] > my_names[j+1]:
-            # Swap the names
-            temp = my_names[j]
-            my_names[j] = my_names[j+1]
-            my_names[j+1] = temp
-print("Names after sorting:")
-print(my_names)
-# Question 3: Bubble Sort for Numbers
-print("\n--- Task 3: Sorting Numbers ---")
-my_nums = [109, 99, 23, 45, 23, 2, 5, 1]
-# Bubble sort logic
-for i in range(len(my_nums)):
-    for j in range(0, len(my_nums) - i - 1):
-        if my_nums[j] > my_nums[j+1]:
-            # Swap the numbers
-            temp = my_nums[j]
-            my_nums[j] = my_nums[j+1]
-            my_nums[j+1] = temp
-print("Numbers after sorting:")
-print(my_nums)
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct FamilyMember {
+    string name;
+    int age;
+};
+
+int main() {
+    int n;
+    cout << "Enter number of family members: ";
+    cin >> n;
+
+    FamilyMember family[100];
+    for (int i = 0; i < n; i++) {
+        cout << "Enter name and age: ";
+        cin >> family[i].name >> family[i].age;
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (family[j].name > family[j + 1].name) {
+                FamilyMember temp = family[j];
+                family[j] = family[j + 1];
+                family[j + 1] = temp;
+            }
+        }
+    }
+
+    string key;
+    cout << "Enter a key name to be searched: ";
+    cin >> key;
+
+    int low = 0, high = n - 1;
+    bool found = false;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (family[mid].name == key) {
+            cout << "name was found and is aged " << family[mid].age << endl;
+            found = true;
+            break;
+        }
+        else if (family[mid].name < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    if (!found) cout << "Not found" << endl;
+
+    string names[] = {"Zuck", "Shayd", "Emely", "Amjed", "Esther", "Manar", "Anne"};
+    int namesSize = 7;
+    for (int i = 0; i < namesSize - 1; i++) {
+        for (int j = 0; j < namesSize - i - 1; j++) {
+            if (names[j] > names[j + 1]) {
+                string temp = names[j];
+                names[j] = names[j + 1];
+                names[j + 1] = temp;
+            }
+        }
+    }
+    cout << "\nSorted Names: ";
+    for (int i = 0; i < namesSize; i++) cout << names[i] << " ";
+    cout << endl;
+
+    int nums[] = {109, 99, 23, 45, 23, 2, 5, 1};
+    int numsSize = 8;
+    for (int i = 0; i < numsSize - 1; i++) {
+        for (int j = 0; j < numsSize - i - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
+    }
+    cout << "\nSorted Numbers: ";
+    for (int i = 0; i < numsSize; i++) cout << nums[i] << " ";
+    cout << endl;
+
+    return 0;
+}
